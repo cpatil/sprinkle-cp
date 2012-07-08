16b043ba0d702e78c8f9b2d343574ac29d5afaf2
Chetan Patil <cpatil@gmail.com>
2012-07-07 19:27:02 -0700

    added sprinkle and yum

---
+++ lib/sprinkle/verifiers/yum.rb
@@ -0,0 +1,21 @@
+module Sprinkle
+  module Verifiers
+    # = Yum package Verifier
+    #
+    # Contains a verifier to check the existance of an Yum package.
+    # 
+    # == Example Usage
+    #
+    #   verify { has_yum 'ntp' }
+    #
+    module Yum
+      Sprinkle::Verify.register(Sprinkle::Verifiers::Yum)
+
+      # Checks to make sure the yum <tt>package</tt> exists on the remote server.
+      def has_yum(package)
+         @commands << "yum list | grep #{package} "
+      end
+
+    end
+  end
+end
